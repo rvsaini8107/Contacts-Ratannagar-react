bab09db353b487dbb2f8ddb7be80a317cd728f2d
ShriGanesh <rvsaini8107@gmail.com>
2023-08-25 11:58:33 +0530

    s

--- src/Data/subCategories.js
+++ src/Data/subCategories.js
@@ -43,7 +43,7 @@ const subCategories = [
             "Railways",
             "Airports",
             "Electrical",
-            "Nagarpalika",
+            "Municipality",
             "Hospitals",
             "Post Offices",
         ]
