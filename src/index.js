929a9b63d796000af299a7f1706caef611dd4991
ShriGanesh <rvsaini8107@gmail.com>
2023-09-04 11:08:19 +0530

    ramram

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+
 import './Pages/pagesStyle.css';
+
 import './Pages/mediaQuery.css';
+import './style.css';
+
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
