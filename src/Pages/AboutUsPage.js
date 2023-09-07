2551e7a572c842cabcdb285f281a90ae1e1272c6
ShriGanesh <rvsaini8107@gmail.com>
2023-09-07 10:34:24 +0530

    up

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import myPic from "./../images/myPic.png"
 
 const AboutUsPage = () => {
   return (
@@ -6,14 +7,14 @@ const AboutUsPage = () => {
       <h2 className="About-heading">About Us</h2>
       <div className="about-main">
         <div className="about-img-div">
-          <img src="" className="aboutus-img" alt="" />
+          {myPic &&<img src={myPic} className="aboutus-img" alt="" />}
         </div>
         <div className="about-p-div">
           <p className="about-p">
-            मैं श्री गणेश सैनी अपने Contacts Ratannagar website में आपका स्वागत
-            करता हूं। यह website रतन नगर के निवासियों को सुविधा प्रदान करने के
-            लिए बनाया गया है। इसमें आपको कोई त्रुटि या कुछ सुझाव देना हो या कुछ
-            और नंबर जोड़ना हो तो कांटेक्ट करें ऊपर दिए गए नंबरों पर | <br />
+            मैं श्रीगणेश सैनी अपने Contacts Ratannagar website में आपका स्वागत
+            करता हूं। <br/>यह website रतन नगर के निवासियों को सुविधा प्रदान करने के
+            लिए बनाया गया है। <br/>इसमें आपको कोई त्रुटि या कुछ सुझाव देना हो या कुछ
+            और नंबर जोड़ना हो तो Contact करें ऊपर दिए गए नंबरों पर | <br />
             धन्यवाद,
           </p>
         </div>
